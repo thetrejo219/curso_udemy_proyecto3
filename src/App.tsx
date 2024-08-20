a8dcfa2e744986c6776d52df69af83e34329eb88
thetrejo018 <juandiego6912@outlook.com>
2024-08-20 16:27:46 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+import Header from "./components/Header"
+import Guitar from "./components/Guitar"
+import { useCart } from "./hooks/useCart"
+function App() {
+  const {
+    data,
+       cart,addToCart,
+       clearCart,
+       decrementar,
+       increaseQuantity,
+       removeFromCart,
+       isEmpty,
+       cartTotal
+  }= useCart()
+  
+  return (
+    <>
+    <Header cart={cart} removeFromCart={removeFromCart} increaseQuantity={increaseQuantity}
+    decrementar={decrementar} clearCart={clearCart} isEmpty={isEmpty} cartTotal={cartTotal}
+    />
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+        {data.map((elemento)=>
+          (
+            <Guitar
+            key={elemento.id}
+            guitar={elemento}
+            addToCart={addToCart}
+            />    
+          )
+        )}    
+        
+            
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default App
